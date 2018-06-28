0053a0f8c3577d79939f162f15cc02e0162039e0
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-28 16:19:10 -0400

    Update ex1.js

--- chapter 02/ex1.js
+++ chapter 02/ex1.js
@@ -1,3 +1,16 @@
+/*
+Escreva um programa que faça sete chamadas a console.log() para retornar o seguinte triângulo:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
 var tri = '';
 for(var i = 1; i <= 7; i++){
 	tri += '#';
