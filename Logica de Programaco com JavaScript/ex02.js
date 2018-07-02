6f57037b0746ed5bdcab8fbb7f47e069fd4e2b78
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-02 18:55:41 -0400

    Create ex02.js

---
+++ Logica de Programaco com JavaScript/ex02.js
@@ -0,0 +1,23 @@
+//pedir para o usuario digitar um numero
+//converter em numero inteiro
+var num1 = parseInt(prompt('Digite um número: '));
+                                    
+//pedir para o usuario digitar outro numero
+//converter em numero inteiro
+var num2 = parseInt(prompt('Digite outro número: '));
+                        
+//mostrar a subtração concatenando com o texto "a subtração é: ..."
+var subtracao = num1 + num2;
+alert('A subtração é '+ subtracao);
+                        
+//mostrar a multiplicação concatenando com o texto "a multiplicação é: ..."
+var multiplicacao = num1 * num2;
+alert('A multiplicação é: '+ multiplicacao);
+
+//mostrar a divisão concatenando com o texto "a divisão é: ..."
+var divisao = num1 / num2;
+alert('A divisão é: '+ divisao);
+
+//mostrar a soma concatenando com o texto "a soma é: ..."
+var soma = num1 + num2;
+alert('A soma dos números é '+ soma);
