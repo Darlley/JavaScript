39b4dc5f7d52d024fdf9491f042b9315844f7cea
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-03 10:25:19 -0400

    Update ex14.js

--- Logica de Programaco com JavaScript/ex14.js
+++ Logica de Programaco com JavaScript/ex14.js
@@ -2,14 +2,27 @@
 Criar array com três objetos. Cada objeto deverá ter duas propriedades (nome e email). Depois, basta mostrar na tela o nome e o email de cada um dos objetos do array criado.
 */
 
-var produto1 = new Object();
-produto1.nome = 'Caneta';
-produto1.preco = 2.2;
-produto1.estoque = 10;
+  var person1 = new Object();
+                person1.nome = 'Naturo'
+                person1.email = 'Naruto@gmail.com'
 
-var msg = ''
-msg += 'Nome: '+ produto1.nome + '</br>';
-msg += 'Preço: '+ produto1.preco + '</br>';
-msg += 'Estoque: '+ produto1.estoque + '</br>';
+var person2 = new Object();
+                person2.nome = 'Goku'
+                person2.email = 'Goku@gmail.com'
+             
+var person3 = new Object();
+                person3.nome = 'Saytama'
+                person3.email = 'Saytama@gmail.com'
+
+var persons = new Arrar(person1, person2, person3)
+console.log(persons)
+                
+var msg = '';
+var x = 0;
+while(persons[x]){
+  console.log(persons[x].nome)
+  msg += persons[x].nome + ' - ' + persons[x].email + '</br>'
+  x++;
+}
 
 document.querySelector('#output').innerHTML = msg;
