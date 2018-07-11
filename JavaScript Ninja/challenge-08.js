e74fdc82015c03acf4cd3582b2d761fa91300526
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-11 15:07:16 -0400

    Update challenge-08.js

--- JavaScript Ninja/challenge-08.js
+++ JavaScript Ninja/challenge-08.js
@@ -3,38 +3,42 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(value1, value2){
+  return value1 + value2;
+};
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log("A soma de 5 e 5 é igual a " + sum(5,5) + ".")
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log("O nome da função que faz a soma é " + sum.name + ".")
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName(){
+  return showName.name;
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log("A função " + varShowName.name + " retorna " + varShowName() + ".")
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +52,16 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+function calculator(operador){
+  if(operador === "+" || operador === "-" || operador === "*" || operador === "/" || operador === "%"){
+    function(value1, value2){
+      var result = value1 + operador + value2;
+      return "Resultado da operação: " + value1 + operador + value2 + " = " + result + "."
+    }
+  }else{
+    return "Operação inválida.";
+  }
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
