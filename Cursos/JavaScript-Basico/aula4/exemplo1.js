09e2a863a1d96c13f931e6712569d5ea29a2287f
Darlley <37590954+Darlley@users.noreply.github.com>
2019-09-07 19:05:12 -0400

    Create exemplo1.js

---
+++ Cursos/JavaScript-Basico/aula4/exemplo1.js
@@ -0,0 +1,10 @@
+// comentário simples em uma linha
+
+/* console.log("SAÍDA DE DADOS PELO CONSOLE");
+console.log acima ignorado com comentário de mais de uma linha 
+*/
+
+console.log("SAÍDA DE DADOS PELO CONSOLE");
+console.log("SAÍDA DE DADOS PELO CONSOLE");
+
+// <script src="[CAMINHO DO ARQUIVO JAVASCRIP]"></script> -> Tag para linkar um arquivo js dentro de um arquivo html
