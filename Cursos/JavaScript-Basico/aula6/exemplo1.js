66436aa0f3273828e993b5c46f6357e3daba5e95
Darlley <37590954+Darlley@users.noreply.github.com>
2019-09-07 19:13:56 -0400

    Create exemplo1.js

---
+++ Cursos/JavaScript-Basico/aula6/exemplo1.js
@@ -0,0 +1,9 @@
+var nome = "Var não é mais recomendado pelas boas praticas em programação";
+let Nome = "Javascript é case-sensitive, por tanto: nome, Nome e NOME são diferentes variaveis que podem ter diferentes valores";
+const NOME = "Var e let poder ter seus valores alterados, mas const não muda de valor";
+
+console.log(nome);
+console.log(Nome);
+console.log(NOME);
+
+// Não criam variaveis com o mesmo nome para não atrapalhar a produção
