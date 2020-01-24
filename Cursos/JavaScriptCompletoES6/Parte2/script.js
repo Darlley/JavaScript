d4d84ed94f5ffa3c522ed1d84ede81cfb420c8f2
Darlley <37590954+Darlley@users.noreply.github.com>
2020-01-24 12:02:32 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte2/script.js
@@ -0,0 +1,15 @@
+console.log("E X E R C Í C I O S");
+
+var $Nome = "Darlley";
+var $Idade = 21;
+var $Comida;
+$Comida = "Panqueca";
+
+var variavel1,
+    variavel2,
+    variavel3,
+    variavel4,
+    variavel5;
+
+
+console.log($Nome, $Idade, $Comida);
