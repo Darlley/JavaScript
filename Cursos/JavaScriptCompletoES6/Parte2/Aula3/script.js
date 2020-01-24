540ad47a409963121c4b2afa2fd02303c5848583
Darlley <37590954+Darlley@users.noreply.github.com>
2020-01-24 12:05:26 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte2/Aula3/script.js
@@ -0,0 +1,27 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; //R: 35
+console.log(total);
+
+
+// Crie duas expressões que retornem NaN
+var expressao1 = "Frase 1" / 1;
+var expressao2 = "Frase 2" - 2;
+
+console.log(expressao1);
+console.log(expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200';
+console.log(soma+50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var pre_incremento = 5;
+console.log(++pre_incremento);
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+var pesoPorDois = (+numero/2) + unidade;
+console.log(pesoPorDois);
