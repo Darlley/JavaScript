980f0bdeda22377ddee0098ca34d70730cc1ae16
Darlley <37590954+Darlley@users.noreply.github.com>
2020-01-24 12:04:23 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte2/Aula2/script.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+var nome = "Darlley";
+
+// Declare uma variável contendo um número dentro de uma string
+var nove = "9";
+
+// Declare uma variável com a sua idade
+var idade = 21;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "Brito";
+var nomeCompleto = `Nome completo: ${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time';
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
