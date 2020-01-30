3bdc72b4679bee8a26f34da100590fcb57dfb030
Darlley <37590954+Darlley@users.noreply.github.com>
2020-01-30 10:44:38 -0400

    Update script.js

--- Cursos/JavaScriptCompletoES6/Parte3/Aula09/script.js
+++ Cursos/JavaScriptCompletoES6/Parte3/Aula09/script.js
@@ -1,18 +1,23 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo');
+const classAtivo = "ativo";
 
-if(tabMenu.length && tabContent.length){
-    function activetab(index){
-        tabContent.forEach(section => {
-            section.classList.remove('ativo');
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    tabContent[0].classList.add(classAtivo);
+
+    if(tabMenu.length && tabContent.length){
+        function activetab(index){
+            tabContent.forEach(section => {
+                section.classList.remove(classAtivo);
+            });
+            tabContent[index].classList.add(classAtivo);
+        }
+
+        tabMenu.forEach((item, index) => {
+            item.addEventListener('click', function(){
+                activetab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
     }
-
-    tabMenu.forEach((item, index) => {
-        item.addEventListener('click', function(){
-            activetab(index);
-        });
-    });
 }
+initTabNav();
