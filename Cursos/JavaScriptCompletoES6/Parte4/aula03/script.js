48b0275a65b78e3e436446fd5b4c7d0aa1247d0f
Darlley <37590954+Darlley@users.noreply.github.com>
2020-02-03 10:58:05 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte4/aula03/script.js
@@ -0,0 +1,20 @@
+/*
+Liste 5 objetos nativos
+1- Object
+2- String
+3- Array
+4- Function
+5- Number
+
+Liste 5 objetos do browser
+1- NodeList
+2- HTMLCollection
+3- Element
+5- Document
+
+
+Liste 2 Métodos, Propriedades ou Objetos
+presentes no Chrome que não existem no Firefox
+
+webkit chrome
+*/
