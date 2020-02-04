f16fde80b5cdc4a63e8bd2f79302fb299731efee
Darlley <37590954+Darlley@users.noreply.github.com>
2020-02-04 09:18:00 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte4/aula05/script.js
@@ -0,0 +1,25 @@
+// Retorne um número aleatório
+// entre 1050 e 2000
+console.log(Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050);
+
+
+// Retorne o maior número da lista abaixo
+let numeros = '4, 5, 20, 8, 9';
+numeros = numeros.split(', ');
+console.log(Math.max(...numeros)); /// "..." percorre o array
+ 
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
+function limparPreco(preco){
+	preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+	preco = +preco.toFixed(2);
+	return preco;
+}
+let soma = 0;
+listaPrecos.forEach(preco => {
+	soma += limparPreco(preco);
+});
+console.log(soma);
