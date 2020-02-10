ce65d460a9497fbf2b7884c0a912ccf7f62a1ad2
Darlley <37590954+Darlley@users.noreply.github.com>
2020-02-10 09:25:29 -0400

    Create script.js

---
+++ Cursos/JavaScriptCompletoES6/Parte5/aula02/js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from './modules/tab-nav.js';
+import initAccordion from './modules/accordion.js';
+import initScroll from './modules/scroll-suave.js';
+import initAnimaScroll from './modules/scroll-animacao.js';
+
+initTabNav();
+initAccordion();
+initScroll();
+initAnimaScroll();
