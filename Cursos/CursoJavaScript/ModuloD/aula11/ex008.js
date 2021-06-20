c7aba686be8655bd654a85ff35efeb957791ddfd
Darlley <37590954+Darlley@users.noreply.github.com>
2021-06-19 21:30:25 -0400

    aula 11

---
+++ Cursos/CursoJavaScript/ModuloD/aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 50.5
+console.log(`A velocidade do carro é ${velocidade}km/h`)
+if (velocidade > 60 ){
+    console.log(`Você foi multado!!!`)
+}else{
+    console.log(`boa viagem!`)
+}
