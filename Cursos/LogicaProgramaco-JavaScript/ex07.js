223f0f3d7aa056a10dd2fae6964aac7e126f930c
Darlley <37590954+Darlley@users.noreply.github.com>
2021-08-22 17:32:22 -0400

    Delete Cursos/LogicaProgramaco-JavaScript directory

--- Cursos/LogicaProgramaco-JavaScript/ex07.js
+++
@@ -1,3 +0,0 @@
-for(var i=1004; i<=2017; i+=4){
-  document.write(i+'</br>'+'-------'+'</br>')
-}
--- Cursos/LogicaProgramaco-JavaScript/ex12.js
+++
@@ -1,5 +0,0 @@
-var dias = new Array('Domingo', 'Segunda','Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado')
-var hoje = new Date();
-var diaSemana = hoje.getDay();
-
-alert('Hoje é '+ dias[diaSemana])
